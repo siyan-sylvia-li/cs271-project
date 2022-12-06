1bc7564884de433934234ec852fcb1e1fcf256cb
siyan-sylvia-li <lisiyansylvia@gmail.com>
2022-12-06 13:48:43 -0800

    Newest commands

--- run_scibert.sh
+++ run_scibert.sh
@@ -1,6 +1,6 @@
-python3 ft_pheno.py --lr=0.00001 --ft_mode=classifier --batch_size=8 --bert_name=allenai/scibert_scivocab_cased --exp_name=scibert --lstm_head
-python3 ft_pheno.py --lr=0.00001 --ft_mode=lastbert --batch_size=8 --bert_name=allenai/scibert_scivocab_cased --exp_name=scibert --lstm_head
-python3 ft_pheno.py --lr=0.00001 --ft_mode=all --batch_size=8 --bert_name=allenai/scibert_scivocab_cased --exp_name=scibert --lstm_head
-python3 ft_pheno.py --lr=0.00001 --ft_mode=classifier --batch_size=8 --bert_name=allenai/scibert_scivocab_cased --exp_name=scibert
-python3 ft_pheno.py --lr=0.00001 --ft_mode=lastbert --batch_size=8 --bert_name=allenai/scibert_scivocab_cased --exp_name=scibert
-python3 ft_pheno.py --lr=0.00001 --ft_mode=all --batch_size=8 --bert_name=allenai/scibert_scivocab_cased --exp_name=scibert
+python3 ft_pheno.py --lr=0.00001 --ft_mode=classifier --batch_size=8 --bert_name=allenai/scibert_scivocab_cased --exp_name=scibert+lstm --lstm_head --early_stopping
+python3 ft_pheno.py --lr=0.00001 --ft_mode=lastbert --batch_size=8 --bert_name=allenai/scibert_scivocab_cased --exp_name=scibert+lstm --lstm_head --early_stopping
+python3 ft_pheno.py --lr=0.00001 --ft_mode=all --batch_size=8 --bert_name=allenai/scibert_scivocab_cased --exp_name=scibert+lstm --lstm_head --early_stopping
+python3 ft_pheno.py --lr=0.00001 --ft_mode=classifier --batch_size=8 --bert_name=allenai/scibert_scivocab_cased --exp_name=scibert --early_stopping
+python3 ft_pheno.py --lr=0.00001 --ft_mode=lastbert --batch_size=8 --bert_name=allenai/scibert_scivocab_cased --exp_name=scibert --early_stopping
+python3 ft_pheno.py --lr=0.00001 --ft_mode=all --batch_size=8 --bert_name=allenai/scibert_scivocab_cased --exp_name=scibert --early_stopping
