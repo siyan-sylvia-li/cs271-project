bb525bd5672583f500c6196570a8d5faeac6cf75
FeiyangYu <kathyyufeiyang@gmail.com>
2022-12-11 10:08:15 -0800

    Removed .ipynb_checkpoints from git tracking

--- .ipynb_checkpoints/run_discharge-checkpoint.sh
+++
@@ -1,7 +0,0 @@
-python3 ft_pheno.py --lr=0.00001 --ft_mode=classifier --batch_size=8 --bert_name=emilyalsentzer/Bio_Discharge_Summary_BERT --exp_name=discharge_bert+lstm --lstm_head --early_stopping
-python3 ft_pheno.py --lr=0.00001 --ft_mode=lastbert --batch_size=8 --bert_name=emilyalsentzer/Bio_Discharge_Summary_BERT --exp_name=discharge_bert+lstm --lstm_head --early_stopping
-python3 ft_pheno.py --lr=0.00001 --ft_mode=all --batch_size=8 --bert_name=emilyalsentzer/Bio_Discharge_Summary_BERT --exp_name=discharge_bert+lstm --lstm_head --early_stopping
-python3 ft_pheno.py --lr=0.00001 --ft_mode=classifier --batch_size=8 --bert_name=emilyalsentzer/Bio_Discharge_Summary_BERT --exp_name=discharge_bert --early_stopping
-python3 ft_pheno.py --lr=0.00001 --ft_mode=lastbert --batch_size=8 --bert_name=emilyalsentzer/Bio_Discharge_Summary_BERT --exp_name=discharge_bert --early_stopping
-python3 ft_pheno.py --lr=0.00001 --ft_mode=all --batch_size=8 --bert_name=emilyalsentzer/Bio_Discharge_Summary_BERT --exp_name=discharge_bert --early_stopping
-
